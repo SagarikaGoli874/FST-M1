c1cf175a64c757456e9c8b2b6a96986873494b51
SagarikaGoli874 <93118278+SagarikaGoli874@users.noreply.github.com>
2021-12-03 22:18:26 +0530

    Add files via upload

--- Java/Activities/Activity8.java
+++ Java/Activities/Activity8.java
@@ -1,38 +1,24 @@
 package activities;
-
-
-class CustomException extends Exception
-{
-private String message=null;
-public CustomException(String msg)
-	{
-	 message=msg;
-	}
-
-@Override
-public String getMessage() {
-    return message;
-}
-
-}
 public class Activity8 {
-	public static void main(String[] args)
-	{
-	  Activity8 obj=new Activity8();
-	  try {
-		obj.exceptionTest(null);
-	} catch (CustomException e) {
-		// TODO Auto-generated catch block
-		e.printStackTrace();
-	}
-	}
-	
-	public void exceptionTest(String s) throws CustomException {
-        if(s == null) {
-            throw new CustomException("String value is null");
-        } else {
-            System.out.println(s);
-        }
 
-}
-}
+	public static void main(String[] args) {
+        try {
+ 
+            Activity8.exceptionTest("Will print to console");
+            Activity8.exceptionTest(null); 
+	            Activity8.exceptionTest("Won't execute");
+	        } catch(CustomException one) {
+	            System.out.println("Inside catch block: " + one.getMessage());
+	        }
+	    }
+	
+    static void exceptionTest(String str) throws CustomException {
+	        if(str == null) {
+	            throw new CustomException("String value is null");
+	        } else {
+	            System.out.println(str);
+	        }
+	    }
+
+	}
+
--- Java/Activities/Activity9.java
+++ Java/Activities/Activity9.java
@@ -1,27 +1,34 @@
 package activities;
+
 import java.util.ArrayList;
 
 public class Activity9 {
-    public static void main(String[] args) {
-        ArrayList<String> al = new ArrayList<String>();
-        //Adding objects to Array List at default index
-        al.add("name1");
-        al.add("name2");
-        al.add("name3");
-        al.add("name4");
-        al.add("name5");
-   
-        for(int i=0;i<al.size()-1;i++)
-        {
-            System.out.println("The element at "+i+"th location is "+al.get(i));
-        }
-        
-        System.out.println("3rd element is" + al.get(2));
-        System.out.println("Is name1 exists " + al.contains("name1"));
-        System.out.println("Size of ArrayList is " + al.size());
-        
-        al.remove("name5");
-        
-        System.out.println("New Size of ArrayList: " + al.size());
-    }
+
+	public static void main(String[] args) {
+
+		ArrayList<String> myList = new ArrayList<String>();
+		myList.add("ram");
+		myList.add("Sham");
+		myList.add("Diya");
+		myList.add("Raju");
+		myList.add("Bheem");
+		System.out.println("Print All names:");
+
+		for (String a : myList) {
+
+			System.out.println(a);
+
+		}
+		System.out.println("3rd element: " + myList.get(3));
+		System.out.println("do we have ram in our batch: " + myList.contains("ram"));
+		System.out.println("size: " + myList.size());
+		myList.remove(2);
+		System.out.println("Print All names:");
+		for (String a : myList) {
+
+			System.out.println(a);
+
+		}
+	}
+
 }
